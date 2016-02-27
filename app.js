42b6ae0e4183d551df2ef1b2691f858468616ca1
Ben Halverson <benhalverson33@gmail.com>
2016-02-27 11:22:00 -0800

    create articleComponent

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ System.register(['angular2/platform/browser', 'angular2/core'], function(exports
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var browser_1, core_1;
-    var reddit;
+    var ArticleComponent, reddit;
     return {
         setters:[
             function (browser_1_1) {
@@ -21,19 +21,41 @@ System.register(['angular2/platform/browser', 'angular2/core'], function(exports
                 core_1 = core_1_1;
             }],
         execute: function() {
+            ArticleComponent = (function () {
+                function ArticleComponent() {
+                    this.title = 'Angular 2';
+                    this.link = 'http://angular.io';
+                    this.votes = 10;
+                }
+                ArticleComponent.prototype.voteUp = function () {
+                    this.votes += 1;
+                };
+                ArticleComponent.prototype.voteDown = function () {
+                    this.votes -= 1;
+                };
+                ArticleComponent = __decorate([
+                    core_1.Component({
+                        selector: 'reddit-article',
+                        host: {
+                            class: 'row'
+                        },
+                        template: "\n    <div class=\"four wide column center aligned votes\">\n      <div class=\"ui statistic\">\n        <div class=\"value\">\n          {{ votes }}\n        </div>\n        <div class=\"label\">\n          Points\n        </div>\n      </div>\n    </div>\n    <div class=\"twelve wide column\">\n      <a class=\"ui large header\" href=\"{{ link }}\"\n      {{ title }}\n      </a>\n      <ul class=\"ui big horizontal list voters\">\n        <li class=\"item\"\n          <a href (click)=\"voteUp()\">\n            <i class=\"arrow up icon\"></i>\n            upvote\n          </a>\n        </li>\n        <li class=\"item\"\n          <a href (click)=\"voteDown()\">\n            <i class=\"arrow down icon\"></i>\n            downvote\n          </a>\n        </li>\n      </ul>\n    </div>\n  "
+                    }),
+                    core_1.Component({
+                        selector: 'reddit',
+                        directives: [ArticleComponent],
+                        template: "\n  <form class=\"ul large form segment\">\n    <h3 class=\"ui header\">Add a Link</h3>\n    <div class=\"field\">\n      <label for=\"title\">Title:</label>\n      <input name=\"title\" #newtitle>\n    </div>\n    <div class=\"field\">\n      <label for=\"link\">Link:</label>\n      <input name=\"link\" #newlink>\n    </div>\n\n\n  <button (click)=\"addArticle(newtitle, newlink)\" class=\"ui positive right floated button\">\n    Submit link\n  </button>\n  </form>\n\n  <div class=\"ui grid posts\">\n    <reddit-article>\n    </reddit-article>\n  </div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ArticleComponent);
+                return ArticleComponent;
+            }());
             reddit = (function () {
                 function reddit() {
                 }
                 reddit.prototype.addArticle = function (title, link) {
                     console.log("Adding article title: " + title.value + " and link: " + link.value);
                 };
-                reddit = __decorate([
-                    core_1.Component({
-                        selector: 'reddit',
-                        template: "\n  <form class=\"ul large form segment\">\n    <h3 class=\"ui header\">Add a Link</h3>\n    <div class=\"field\">\n      <label for=\"title\">Title:</label>\n      <input name=\"title\">\n    </div>\n    <div class=\"field\">\n      <label for=\"link\">Link:</label>\n      <input name=\"link\">\n    </div>\n\n\n  <button (click)=\"addArticle(newTitle, newlink)\" class=\"ui positive right floated button\">\n    Submit link\n  </button>\n  </form>\n  "
-                    }), 
-                    __metadata('design:paramtypes', [])
-                ], reddit);
                 return reddit;
             }());
             browser_1.bootstrap(reddit);
